9e193564ee25bfd071592742bfa4d2c9885031f4
kaladivyaprada <136182825+kaladivyaprada@users.noreply.github.com>
2024-10-30 12:19:50 +0530

    Update main.c

--- ferror_function_infile/main.c
+++ ferror_function_infile/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    FILE *fp;
+    char ch;
+    fp = fopen("Simple.txt","w");
+    fputc('A',fp);
+    rewind(fp);
+    ch = fgetc(fp);
+    if(ferror(fp)){
+        printf("Error in reading from file");
+    }
+    else{
+        printf("ch= %c",ch);
+        fclose(fp);
+    }
     return 0;
 }
